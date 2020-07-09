4462fe1637d9bae45d0c6d7bd78d3871415044b1
Jeselle Bacosmo <jbacosmo@gmail.com>
2020-07-09 14:14:23 +0800

    change footer

--- resources/js/components/footer/Footer.vue
+++ resources/js/components/footer/Footer.vue
@@ -3,7 +3,7 @@
         <div class="pull-right hidden-xs">
             <b>Version</b> 2.4.13
         </div>
-        <strong>Copyright © 2014-2019 <a href="https://adminlte.io">AdminLTE</a>.</strong> All rights
+        <strong>Copyright © 2020 <a href="http://tools.stalinks.com">StaLink System</a>.</strong> All rights
         reserved.
     </footer>
 </template>
