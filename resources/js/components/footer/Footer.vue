b68badca6ee4a0c815807c630b99eb9ee2cf78be
Jeselle Bacosmo <jbacosmo@gmail.com>
2020-07-09 16:53:59 +0800

    fixed employee type

--- resources/js/components/footer/Footer.vue
+++ resources/js/components/footer/Footer.vue
@@ -3,7 +3,7 @@
         <div class="pull-right hidden-xs">
             <b>Version</b> 2.4.13
         </div>
-        <strong>Copyright © 2020 <a href="http://tools.stalinks.com">StaLink System</a>.</strong> All rights
+        <strong>Copyright © 2020 <a href="https://www.stalinks.com">StaLink System</a>.</strong> All rights
         reserved.
     </footer>
 </template>
