d610b589fc2ed2d2e1507944ec45f9d96b317808
Jeselle Bacosmo <jeselle.marketingcrossmedia@gmail.com>
2020-07-16 11:21:22 +0800

    added filter on purchase

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -7,6 +7,7 @@ use App\Models\Backlink;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Registration;
 use App\Models\Publisher;
+use App\Models\BuyerPurchased;
 
 class PurchaseController extends Controller
 {
@@ -17,6 +18,8 @@ class PurchaseController extends Controller
      */
     public function getList(Request $request) {
         $user = Auth::user();
+        $filter = $request->all();
+
         $list = Backlink::with(['publisher' => function($query){
             $query->with('user:id,name');
         }, 'user'])
@@ -27,8 +30,43 @@ class PurchaseController extends Controller
             $list->where('user_id', $user->id);
         }
 
+        if( isset($filter['payment_status']) && $filter['payment_status'] != ''){
+            $list->where('payment_status', $filter['payment_status']);
+        }
+
+        if(isset($filter['status']) && !is_null($filter['status'])) {
+            $list->where('status', $filter['status']);
+        }
+
+        if( isset($filter['buyer']) && $filter['buyer'] != ''){
+            $buyer = $filter['buyer'];
+            $list->whereHas('user', function($query) use ($buyer){
+                return $query->where('id', $buyer);
+            });
+        }
+
+        if( isset($filter['seller']) && $filter['seller'] != ''){
+            $seller = $filter['seller'];
+            $list->whereHas('publisher', function($query) use ($seller){
+                return $query->where('user_id', $seller);
+            });
+        }
+
+        $getBuyer = BuyerPurchased::select('buyer_purchased.user_id_buyer','users.username')
+                                ->leftJoin('users', 'users.id', '=', 'buyer_purchased.user_id_buyer')
+                                ->where('buyer_purchased.status', 'Purchased')
+                                ->groupBy('buyer_purchased.user_id_buyer', 'users.username')
+                                ->get();
+
+        $getSeller = Publisher::select('publisher.user_id', 'users.username')
+                            ->leftJoin('users', 'users.id', '=', 'publisher.user_id')
+                            ->groupBy('publisher.user_id', 'users.username')
+                            ->get();
+
         return [
-            'data' => $list->get()
+            'data' => $list->get(),
+            'buyers' => $getBuyer,
+            'sellers' => $getSeller
         ];
     }
 }
