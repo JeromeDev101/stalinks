0a5aca3da377d55c7b9db68f1dbe07b8f48bef8b
Morley <moravel752@gmail.com>
2020-10-29 12:51:10 +0800

    mailgun api spacing

--- routes/api.php
+++ routes/api.php
@@ -182,8 +182,8 @@ Route::middleware('auth:api')->group(function () {
 
 
 //Mailgun
-    Route::group(['prefix'=> 'mail'], function(){
-        Route::name('email_send')->post('/send','MailgunController@send');
-        Route::name('email_retrieve_all')->get('/retrieve-all','MailgunController@retrieve_all');
-        Route::name('message_view')->post('/view-message','MailgunController@view_message');
-    });
+Route::group(['prefix'=> 'mail'], function(){
+    Route::name('email_send')->post('/send','MailgunController@send');
+    Route::name('email_retrieve_all')->get('/retrieve-all','MailgunController@retrieve_all');
+    Route::name('message_view')->post('/view-message','MailgunController@view_message');
+});
