0eb22d40fa6e623a683fcb425b30a85f4bfef9f5
Morley <moravel752@gmail.com>
2020-12-03 10:06:10 +0800

    check both attachment and attachment-1

--- app/Http/Controllers/MailgunController.php
+++ app/Http/Controllers/MailgunController.php
@@ -315,8 +315,17 @@ $description = 'Test route';
 
     public function post_reply(Request $request)
     {
-      
-        $attch_obj = json_decode($request->attachments)[0];
+       
+        //return response()->json($request->all());
+       
+       // dd(json_decode($request->attachments)[0]);
+        //dd("exit");
+        if( isset($request->attachments) )
+        {
+           $attch_obj = json_decode($request->attachments)[0]; 
+        }else {
+             $attch_obj = json_decode($request->only('attachment-1'))[0]; 
+        } 
         
         DB::table('test_replies')->insert(['alldata' => json_encode($request->all())]);   
         $data = [
