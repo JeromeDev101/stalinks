03e7cce9e23339778a48f529052a9f70902b416f
Morley <moravel752@gmail.com>
2020-12-04 14:09:01 +0800

    date to date

--- resources/js/modules/mails/views/Maillog.vue
+++ resources/js/modules/mails/views/Maillog.vue
@@ -69,7 +69,7 @@
                                         <td>{{ log.from }}</td>
                                         <td>{{ log.to }}</td>
                                         <td v-html="statusLabel(log.status)"></td>
-                                        <td>{{ log.to }}</td>
+                                        <td>{{ log.date }}</td>
                                     </tr>
                                 </tbody>
                             </table>
