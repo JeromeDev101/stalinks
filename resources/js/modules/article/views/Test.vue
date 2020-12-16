197f5d1d325b6316fa313cb275df3e13db2e7ebb
Morley <moravel752@gmail.com>
2020-12-16 14:16:07 +0800

    log e

--- resources/js/modules/article/views/Test.vue
+++ resources/js/modules/article/views/Test.vue
@@ -17,7 +17,7 @@ export default {
     mounted(){
         Echo.channel('morley') //Should be Channel Name
             .listen('LikeEvent', (e) => {
-               
+               console.log(e);
 
                 if(this.user.id == e.user_id){
                      alert(e.message);
