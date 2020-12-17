ad3da3cb3770244303ba437b6ccb7865753e8508
Morley <moravel752@gmail.com>
2020-12-17 15:46:05 +0800

    Desc2

--- resources/views/paypal.blade.php
+++ resources/views/paypal.blade.php
@@ -30,7 +30,7 @@
         <div class="flex-center position-ref full-height">
   
             <div class="content">
-                <h1>Laravel 5.8 PayPal Integration Tutorial - ItSolutionStuff.com</h1>
+                <h1>Morley Paypal Setup</h1>
                   
                 <table border="0" cellpadding="10" cellspacing="0" align="center"><tr><td align="center"></td></tr><tr><td align="center"><a href="https://www.paypal.com/in/webapps/mpp/paypal-popup" title="How PayPal Works" onclick="javascript:window.open('https://www.paypal.com/in/webapps/mpp/paypal-popup','WIPaypal','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=1060, height=700'); return false;"><img src="https://www.paypalobjects.com/webstatic/mktg/Logo/pp-logo-200px.png" border="0" alt="PayPal Logo"></a></td></tr></table>
   
