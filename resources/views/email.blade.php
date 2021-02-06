27b8da46a082a82ed25d9a0a1e30a9f18d207a63
jerome-apacaff <jerome.marketingcrossmedia@gmail.com>
2021-02-06 09:40:14 -0800

    check image in email template v2

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -105,7 +105,7 @@
     </style>
 </head>
 
-<body style="background-image: {{ asset('/images/background-login.jpg') }}; background-repeat: no-repeat;background-attachment: fixed;background-size: cover;">
+<body style="background-image: url('{{ asset('/images/background-login.jpg') }}'); background-repeat: no-repeat;background-attachment: fixed;background-size: cover;">
     <!-- HIDDEN PREHEADER TEXT -->
     <div style="display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: 'Lato', Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;"> We're thrilled to have you here! Get ready to dive into your new account. </div>
     <table border="0" cellpadding="0" cellspacing="0" width="100%">
