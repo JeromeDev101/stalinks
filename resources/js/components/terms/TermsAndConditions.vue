affc19edbba4c3537fd383af5e9f850cbd8e6f14
Adrian Pabe <adrianpabs14@gmail.com>
2021-03-18 10:10:44 +0800

    Add terms and condition component

---
+++ resources/js/components/terms/TermsAndConditions.vue
@@ -0,0 +1,168 @@
+<template>
+    <div class="modal fade" id="modalTermsAndCondition" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Terms and Condition</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <h2>Information that STALinks Collect</h2>
+                    <p>&#8211; Information data that you have registered from STALinks.com, this includes; name, email address, phone<br />
+                        number.<br />
+                        &#8211; STALinks use cookies and/or web beacons to enhance experience, gather general visitor information, track<br />
+                        visits to our site.</p>
+                    <h2>What do we use your Information for?</h2>
+                    <p>&#8211; To personalize client’s experience<br />
+                        =&gt; This helps us to be better at assisting you for your interests.<br />
+                        &#8211; To improve our customer service<br />
+                        =&gt; This helps us to effectively respond to your requests, support needs, and complaints.<br />
+                        &#8211; To send updates on our sites that based on your interests.<br />
+                        =&gt; We will send you an email based on your account from the registration.<br />
+                        *How do we protect your personal information?<br />
+                        &#8211; We implement a variety of security to maintain the safety of client’s personal information and orders.<br />
+                        *Do we disclose any information to outside parties?<br />
+                        &#8211; We do not sell, buy, or trade any other transfers to outside parties your personal information. This does<br />
+                        not include third parties who assist us in operating our website and assist you, and so our third parties<br />
+                        agreed to keep all the information is confidential.<br />
+                        However, non-personal identifiable visitor’s information may be provided to other parties for marketing,<br />
+                        advertisement or other uses.</p>
+                    <h2>Children Online Privacy Protector</h2>
+                    <p>&#8211; We do not collect any information from anyone under 13 years of age. Our system and service are all<br />
+                        directed to people who are at least 13 years or older.</p>
+                    <h2>Online Privacy Policy</h2>
+                    <p>&#8211; We only collected information throughout the website and we do not collect any other information by<br />
+                        offline/outside our system.</p>
+                    <h2>Terms, Payments and Condition on STALinks</h2>
+                    <p>The terms of service agreement set by the standard of STALinks.com service. By using STALinks.com<br />
+                        service, you must agree to the terms, payments and conditions. If you do not agree to the terms, payments<br />
+                        and conditions, you will immediately cease all the usage of STALinks.com.</p>
+                    <h2>Description of Service</h2>
+                    <p>Our site provides an online service for making our clients easier to buy or sell backlinks. Our system will<br />
+                        verify that all backlinks are confirm to our terms and we also reserve to approve and execute of all the<br />
+                        transactions between sellers and buyers.</p>
+                    <h2>Member of Usage</h2>
+                    <p>&#8211; We will not tolerate our clients to use our system for any illegal purposes, including violating the security<br />
+                        of any computer network.<br />
+                        &#8211; We will not allow our clients to divulge, record or abuse any information in our website (e.g. inventory,<br />
+                        not limited to URL’s, descriptions and images).<br />
+                        &#8211; STALinks reserves all the rights to determine any links within our system that directed to URL’s that; not<br />
+                        owned, has been deactivated, or cannot be found. Links may be removed without a refund and the account<br />
+                        may be suspended from STALinks.</p>
+                    <h2>Payment made to our clients.</h2>
+                    <p>&#8211; Payment will be made in maximum 48 hours (during working days, exclude; weekends and public<br />
+                        holidays)<br />
+                        -The article must be on live before payment and set up for live at least 12 months based on months on the<br />
+                        calendar.<br />
+                        &#8211; Payment will be transfer by PayPal, Skrill, BTC, WireTransfer, or other any available solution on the<br />
+                        system. (Make sure you have at least one of those account and valid)<br />
+                        &#8211; Payment currency will be done in USD.<br />
+                        &#8211; We are not responsible for any kind of changes during proceeds fees or currency conversion charges and<br />
+                        any kind of taxes, (e.g GST, VST, etc).</p>
+                    <h2>Client account security</h2>
+                    <p>&#8211; After you registered your own account, you will get access to your own control panel.<br />
+                        &#8211; You are responsible for maintaining your username and password that is confidential in our website.<br />
+                        &#8211; You are responsible for your own actions that occur through the use of your account.<br />
+                        &#8211; You agree not to share out your own personal information to outside parties (e.g. username, email<br />
+                        address, password, and transaction)<br />
+                        &#8211; If anything happens to your account (e.g. password stolen, breached or any kind of issue). Please contact<br />
+                        our customer service immediately.<br />
+                        &#8211; STALinks will not responsible for any lost.<br />
+                        &#8211; STALinks reserves to terminate your account without prior notice to you, if you undergo any violations<br />
+                        and others that are against our agreement.</p>
+                    <h2>Client Information</h2>
+                    <p>&#8211; Your account information must be valid (we will not be responsible if you put invalid, inaccurate or<br />
+                        misleading information)<br />
+                        &#8211; Do not add any stolen information.<br />
+                        &#8211; Do not add any third parties&#8217; copyrights, patent, trademark, or property rights or even publicity privacy.<br />
+                        &#8211; Do not violate any laws, statue, or regulations (i.e. without limitation government export control, other<br />
+                        client’s personal information, anti-discrimination, etc)<br />
+                        &#8211; Do not put human trafficking URL’s (e.g. child pornography, harmful to minors, etc)<br />
+                        &#8211; Do not put URL’s that contains; one-click ads or other computer programming routines that are intended to<br />
+                        damage, detrimentally, interfere with surreptitiously intercept or expropriate any system, data or personal<br />
+                        information.<br />
+                        &#8211; Do not create something that could cause STALinks to lose all of data or party.<br />
+                        &#8211; Do not add any links that leads to opposite of STALinks terms.<br />
+                        We will use all of the information you add in our website to help us to identify and verify your account.<br />
+                        We will use your email address for verification of your account on STALinks and send you updates of<br />
+                        backlinks based on your interest in the future.</p>
+                    <h2>Publisher Listings</h2>
+                    <p>All publisher must be agreed to this terms or risk of having your account suspend or deleted. This also may<br />
+                        result in loss of payments and banned from using STALinks system and service.<br />
+                        &#8211; Publisher URL’s must be qualified by our system (we use AHREF standards)<br />
+                        &#8211; Sold URL’s must be exist/visible on the publisher lists at all times (hiding these links we may considered<br />
+                        as system abuse)<br />
+                        &#8211; Publisher URL’s must be complete and valid with working pages and links between pages.<br />
+                        &#8211; URL’s cannot consist harmful content or any illegal materials.<br />
+                        &#8211; Publisher must have valid account and contact information, so that STALinks will contact the publisher for<br />
+                        the payment. (If publisher’s account is invalid, we may delay the payment and possibility to terminate the<br />
+                        account)<br />
+                        &#8211; Publisher must not put links that leads to different URL’s page. ( The URL’s that submitted must leads to<br />
+                        the exact other URL’s of the buyers)<br />
+                        &#8211; Publisher must not put links that contains one-click ads or viruses.<br />
+                        &#8211; Publisher must allow access to STALinks system to validate the backlinks is valid.</p>
+                    <h2>DISCLAIMER OF WARRANTIES</h2>
+                    <p>The service that STALinks provide on an ‘as is’ and on an ‘as available’ basis. STALinks makes no<br />
+                        representation or warranties of any kind, express or implied, regarding the use or the results of our<br />
+                        system in terms of the correctness, accuracy, reability, etc. STALinks do not reserves the liability for any<br />
+                        interruptions in the use of STALinks.</p>
+                    <h2>LIMITATION OF LIABILTY</h2>
+                    <p>STALinka will not take any responsible in any damage, (i.e. loss profits, loss of use, arising out of/or<br />
+                        related to STALinks or the information contained in it, wether if the damage arise in contract, or otherwise.<br />
+                        Even if STALinks has been advised of the possibility of such damages.</p>
+                    <h2>INDEMNIFICATION</h2>
+                    <p>Client must agreed to keep company information at all cost, including from subsidiaries, reasonable<br />
+                        attorney’s fees and costs that made by any other third party due to arising out of client’s use of service.</p>
+                    <h2>COMPOSE AND INTERRUPTION</h2>
+                    <p>STALinks reserves rearrange and update to new service or even discontinue the service with or without<br />
+                        notice to the client. Client must acknowledge and accept that STALinks does not guarantee continuous,<br />
+                        uninterrupted or maintenance of our website might be interfered with/or affected by numerous factors or<br />
+                        circumtances outside of STALinks control.</p>
+                    <h2>PUBLISHER INFORMATION</h2>
+                    <p>Our backlinks/URL’s has been provided by the publishers or collected from available sources. STALinks<br />
+                        always make sure that all the information on the URL is valid. STALinks do no reseveres for other<br />
+                        backlinks that offer by other third party and you must acknowledge that any reliance out of STALinks<br />
+                        service will be on your own risk.</p>
+                    <h2>COMPLIANCE WITH LAWS</h2>
+                    <p>Client must acknowledge of applicable law and compliance with such laws. Client must not use STALinks<br />
+                        service in any way that violates applicable laws, international laws, or other government requirements.<br />
+                        Client must not transmit any material that contains or encourages conduct that triggers constitue criminal<br />
+                        offense, violates any applicable local, state, national, or even international laws or regulations.</p>
+                    <h2>CONFIDENTIAL INFORMATION</h2>
+                    <p>All content that published and existed on STALinks website, including the design, text, graphics, software,<br />
+                        selection and arrangements are property of STALinks and/or third parties that protected by intellectual<br />
+                        property rights. Any use of materials on the website, including modification, replication, any form of data<br />
+                        extraction, or other commercial exploitation of any kind, without prior permission of an authorized written<br />
+                        approval from STALinks official controller is strictly prohibited.<br />
+                        STALinks is a form of the official website. STALinks trademarks might not be used in connection with any<br />
+                        service that is not provided by STALinks in any case that would likely cause the confusion for the clients.</p>
+                    <h2>OTHER TERMS</h2>
+                    <p>By reading all these terms, payments and conditions unlawful, clearheaded, unforced for any reason,<br />
+                        (and any particially enforceable provision) shall remain valid to the possible extention. You agreed to this<br />
+                        terms of service agreement that STALinks provided. This terms of service agreement and other<br />
+                        agreements that has been assigned by STALinks. This terms of service also apply in our discretion, in<br />
+                        addition to not be superseded by, any other written agreement relations between clients and STALinks.<br />
+                        Clients agreed to this terms of service agreement. All clients personal information and other use<br />
+                        described in our Privacy Policy Statements.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal" aria-label="Close">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [],
+
+    data() {
+        return {}
+    },
+
+    methods: {},
+}
+</script>
