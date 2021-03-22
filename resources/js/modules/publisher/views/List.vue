bfa6d865f43cebcbbaf8c6fc9b6afe70370cbd71
Adrian Pabe <adrianpabs14@gmail.com>
2021-03-22 14:11:56 +0800

    Fix continent multi select when null

--- resources/js/modules/publisher/views/List.vue
+++ resources/js/modules/publisher/views/List.vue
@@ -1228,9 +1228,11 @@
                 isPopupLoading: false,
                 filterModel: {
                     // country_id: this.$route.query.country_id || '',
-                    continent_id: this.$route.query.continent_id.map(function (val) {
-                        return parseInt(val, 10);
-                    }) || '',
+                    continent_id: this.$route.query.continent_id
+                        ? this.$route.query.continent_id.map(function (val) {
+                            return parseInt(val, 10);
+                        })
+                        : '',
                     search: this.$route.query.search || '',
                     language_id: this.$route.query.language_id || '',
                     inc_article: this.$route.query.inc_article || '',
