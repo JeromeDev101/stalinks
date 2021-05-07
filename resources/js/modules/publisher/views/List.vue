d7494a33c66ca79c8c2675ca101f3a6766e15a2b
Adrian Pabe <adrianpabs14@gmail.com>
2021-05-07 14:41:33 +0800

    Fix empty inc article on update information in list publisher

--- resources/js/modules/publisher/views/List.vue
+++ resources/js/modules/publisher/views/List.vue
@@ -648,8 +648,8 @@
                                     <label for="">Include Article</label>
                                     <select class="form-control" v-model="updateModel.inc_article">
                                         <option value=""></option>
-                                        <option value="Yes">Yes</option>
-                                        <option value="No">No</option>
+                                        <option value="yes">Yes</option>
+                                        <option value="no">No</option>
                                     </select>
                                 </div>
                             </div>
@@ -2322,7 +2322,7 @@
                     price: that.price,
                     anchor_text: that.anchor_text,
                     link: that.link,
-                    inc_article: that.inc_article,
+                    inc_article: that.inc_article.toLowerCase(),
                     topic: topic,
                     casino_sites: that.casino_sites.toLowerCase(),
                     kw_anchor: that.kw_anchor.toLowerCase(),
