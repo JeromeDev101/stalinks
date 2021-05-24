09baa0ce67c8819e056acd7c65952baf00ac1103
Adrian Pabe <adrianpabs14@gmail.com>
2021-05-24 15:55:24 +0800

    Add user filter on email signatures

--- app/Http/Controllers/MailSignatureController.php
+++ app/Http/Controllers/MailSignatureController.php
@@ -25,6 +25,8 @@ class MailSignatureController extends Controller
     {
         $list = MailSignature::when($request->name, function($query) use ($request){
                 return $query->where( 'name', $request->name);
+            })->when($request->user, function($query) use ($request){
+                return $query->where( 'user_id', $request->user);
             })
             ->with('user')
             ->orderBy('id', 'desc');
