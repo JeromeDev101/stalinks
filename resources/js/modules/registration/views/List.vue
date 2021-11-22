281cd02b6ff315db12e5bf73948309078a2d2398
IvanRalphSTALINKS <78455156+IvanRalphSTALINKS@users.noreply.github.com>
2021-11-22 10:55:38 +0800

    Fixed total amount in notification (#1016)
    Co-authored-by: Ivan Ralph Vitto <vittoivanralph@gmail.com>

--- resources/js/modules/registration/views/List.vue
+++ resources/js/modules/registration/views/List.vue
@@ -96,7 +96,7 @@
 
                             <div class="col-md-3">
                                 <div class="form-group">
-                                    <label>Show Price Basis</label>
+                                    <label>Commission</label>
                                     <select class="form-control" name="" v-model="filterModel.commission">
                                         <option value="">Select Answer</option>
                                         <option value="yes">Yes</option>
@@ -1194,7 +1194,7 @@
 
                             <div class="col-sm-6">
                                 <div class="form-group">
-                                    <label>Show Price Basis <span class="text-danger">*</span></label>
+                                    <label>Commission <span class="text-danger">*</span></label>
                                     <select class="form-control" name="" v-model="accountUpdate.commission">
                                         <option value="yes">Yes</option>
                                         <option value="no">No</option>
