6afa9edfa115d10239902385da1d0dff56e8a522
jerome-apacaff <60372815+jerome-apacaff@users.noreply.github.com>
2021-12-08 11:35:24 +0800

    test ci/cd v2 (#1056)

--- resources/js/modules/dashboard/views/Dashboard.vue
+++ resources/js/modules/dashboard/views/Dashboard.vue
@@ -15,7 +15,7 @@
                 <div class="col-sm-12">
                     <div class="card card-outline card-secondary">
                         <div class="card-header">
-                            <h3 class="card-title text-primary">Total External Domains</h3>
+                            <h3 class="card-title text-primary">Total External Domain</h3>
                             <div class="card-tools">
                             </div>
                         </div>
