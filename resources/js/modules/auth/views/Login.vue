03287695838f9fb0d98b7313a962a7d90626a165
jerome-apacaff <60372815+jerome-apacaff@users.noreply.github.com>
2022-04-05 16:36:28 +0800

    improve login page (#1172)

--- resources/js/modules/auth/views/Login.vue
+++ resources/js/modules/auth/views/Login.vue
@@ -37,39 +37,143 @@
             </div>
         </div>
 
-        <div class="col-md-4 col-md-offset-4 homepage-login__login custom-card">
-            <h2 class="homepage-login__title">
-                <img style="height: auto; width: 385px;" src="../../../../images/stalinks2.png" alt="User Image">
-            </h2>
-            <form class="homepage-login__login-form" @submit.prevent="submitLogin(credentials)">
-                <div class="form-group">
-                    <label for="user-email">{{ $t('message.login.l1') }}</label>
-                    <input v-bind:class="{ 'is-invalid': error && objectNotEmpty(error.email)}" v-model="credentials.email" autofocus="autofocus" type="email" id="user-email" class="form-control" autocomplete="off">
-                    <span v-if="error && objectNotEmpty(error.email)" class="text-danger">{{ error.email[0] }}</span>
-                </div>
-                <div class="form-group mb-5">
-                    <label for="user-password">{{ $t('message.login.l2') }}</label>
-                    <input v-bind:class="{ 'is-invalid': error && objectNotEmpty(error.password)}" v-model="credentials.password" type="password" id="user-password" class="form-control">
-                    <router-link :to="{ path: '/forgot-password' }">
-                        <span>{{ $t('message.login.forgot') }}</span>
-                    </router-link>
-                    <span v-if="error && objectNotEmpty(error.password)"  class="text-danger">{{ error.password[0] }}</span>
-                    <span v-if="error.message"  class="text-danger">{{ error.message }}</span>
-                </div>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary btn-flat float-right"><span><b>{{ $t('message.login.b2') }}</b></span></button>
-                </div>
-                <div class="form-group">
-                    <button type="button" @click="registrationPage" class="btn btn-default btn-flat">{{ $t('message.login.b1') }}</button>
-                </div>
-            </form>
+        <div class="row justify-content-center">
+
+            <div class="card custom-card px-5 pb-5 pt-3">
+                <h2 class="text-center">
+                    <img style="height: auto; width: 320px;" src="../../../../images/stalinks2.png" alt="User Image">
+                </h2>
+
+                <hr/>
+
+                <div class="row">
+                    <div class="col-md-6 d-none d-lg-block d-xl-block">
+                        <div class="h-100 p-4 mr-5 bg1 text-white">
+                            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <h4 class="text-yellow">The First Backlink Desk Management in South Asia</h4>
+                                        <br>
+                                        <p>
+                                            StaLinks is a Digital Marketplace that provides profitable guest posts, backlinks to increase your Google ranking.
+                                        </p>
+                                        <p>
+                                            We connect verified buyers and publishers worldwide with competitive prices, quality, and safety.
+                                        </p>
+                                    </div>
+                                    <div class="carousel-item">
+                                        <h4 class="text-yellow">How does it work</h4>
+                                        <p>
+                                            Our system takes the hassle out of buying and selling links. We made it for you to be fast, easy and secure for whatever you are buyer or seller!  You are no longer have to worry about managing the links you order or finding the right URL’s.
+                                        </p>
+                                        <br>
+                                        <h4 class="text-yellow">Buy Guest Post</h4>
+                                        <p>
+                                            Search from our huge inventory of sites using special filters in just a second you will find the best place for your guest-post. Soon we will give you the options to create your project and our team will do the job for you.
+                                        </p>
+                                    </div>
+                                    <div class="carousel-item">
+                                        <h4 class="text-yellow">Sell Guest Post</h4>
+                                        <p>
+                                            Earn a monthly income from your new articles. Add your sites to our list of publishers and our system will automatically create and monitor the orders for you. Just seat back to received your money.
+                                        </p>
+                                        <br>
+                                        <h4 class="text-yellow">Content Writing</h4>
+                                        <p>
+                                            Writers will deliver your quality “SEO” content. From all around the world we pay them to deliver the best article with native language verified by our quality control team.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a> -->
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="mt-3">
+                            <form @submit.prevent="submitLogin(credentials)">
+                                <h6 class="text-center mb-3">
+                                    {{ $t('message.login.l3') }} 
+                                    <button type="button" @click="registrationPage" class="btn btn-link pt-0 pl-0">{{ $t('message.login.b1') }}</button>
+                                </h6>
+
+                                <div class="input-group mb-4">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">
+                                            <i class="fa fa-envelope"></i>
+                                        </span>
+                                    </div>
+                                    <input :class="{ 'form-control': true, 'is-invalid': error && objectNotEmpty(error.email)}" :placeholder="$t('message.login.l1')" v-model="credentials.email" autofocus="autofocus" type="email" id="user-email" autocomplete="off">
+                                    <span v-if="error && objectNotEmpty(error.email)" class="text-danger">{{ error.email[0] }}</span>
+                                </div>
+                                
+
+                                <div class="input-group mb-5">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">
+                                            <i class="fa fa-lock"></i>
+                                        </span>
+                                    </div>
+                                    <input v-bind:class="{ 'is-invalid': error && objectNotEmpty(error.password)}" :placeholder="$t('message.login.l2')" v-model="credentials.password" type="password" id="user-password" class="form-control">
+                                    <span v-if="error && objectNotEmpty(error.password)"  class="text-danger">{{ error.password[0] }}</span>
+                                    <span v-if="error.message"  class="text-danger">{{ error.message }}</span>
+                                </div>
+
+
+                                <div class="form-group mb-4">
+                                    <button type="submit" class="btn btn-primary btn-lg btn-block"><span><b>{{ $t('message.login.b2') }}</b></span></button>
+                                </div>
+
+                                <div class="form-group mb-5">
+                                    <ul style="padding: 0; margin: 0; text-align: center !important;">
+                                        <li style="display: inline-block; margin: 0 10px 0 10px">
+                                            <a target="_blank" href="https://www.facebook.com/StaLinksGuestPost">
+                                                <span class="fab fa-facebook fa-lg text-default"></span>
+                                            </a>
+                                        </li>
+                                        <li style="display: inline-block; margin: 0 10px 0 10px">
+                                            <a target="_blank" href="https://www.linkedin.com/company/stalinks">
+                                                <span class="fab fa-linkedin fa-lg text-default"></span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                            </form>
+                            <div class="card-footer text-right">
+                                <router-link :to="{ path: '/forgot-password' }">
+                                    <span>{{ $t('message.login.forgot') }}</span>
+                                </router-link>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+
+                
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped>
     .custom-card {
-        border-radius: 8px;
+        width: 1000px;
+    }
+
+    .bg1 {
+        background-image: url("../../../../images/background-login2.jpg");
+        background-color: #cccccc;
+    }
+
+    .text-yellow {
+        color: FEA02F;
     }
 </style>
 
--- resources/lang/Modules/Login/en/main.js
+++ resources/lang/Modules/Login/en/main.js
@@ -1,7 +1,8 @@
 export default {
     l1 : 'Email',
     l2 : 'Password',
+    l3 : 'Don\'t have an account?',
     forgot: 'Forgot Password?',
-    b1: 'Register',
-    b2: 'Login'
+    b1: 'Sign Up',
+    b2: 'Sign In'
 }
