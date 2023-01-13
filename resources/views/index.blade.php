b6b8cf4c00fcfd916d3eaa77bf2836916ed7842e
jerome-apacaff <60372815+jerome-apacaff@users.noreply.github.com>
2023-01-13 10:15:28 +0800

    gtag setup campaign 1 (#1473)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -37,6 +37,17 @@
         gtag('config', 'AW-11019584298');
     </script>
 
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-PQGJGDL5X7"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-PQGJGDL5X7');
+    </script>
+
 </head>
 
     
