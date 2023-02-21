770104a876e3b7c6ade5aeba2d22e360429f50ee
AdrianStalinks <78770464+AdrianStalinks@users.noreply.github.com>
2023-02-21 16:49:20 +0800

    Test gtm setup on live (#1526)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,11 +10,24 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
     <link rel="stylesheet" type="text/css" href="{{ asset('/css/app.css') }}">
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NHZTN38');</script>
+    <!-- End Google Tag Manager -->
+
 </head>
 
 
 
 <body class="sidebar-mini layout-fixed skin-blue">
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NHZTN38"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     <div id="app" class="wrapper">
     </div>
     <script src="https://www.paypal.com/sdk/js?client-id={{ config('paypal.mode') == 'live' ? config('paypal.live.client_id') : config('paypal.sandbox.client_id') }}&currency=USD" data-sdk-integration-source="button-factory"></script>
