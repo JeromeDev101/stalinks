5e21ae8f9ee4409c79c494eb75ffa9bf75cdc1ff
AdrianStalinks <78770464+AdrianStalinks@users.noreply.github.com>
2023-03-17 18:04:30 +0800

    Fix payment label for id (#1535)

--- resources/js/modules/billing/seller/views/List.vue
+++ resources/js/modules/billing/seller/views/List.vue
@@ -335,7 +335,7 @@
                                     </tr>
                                     <tr>
                                         <td colspan="2">
-                                            ID:
+                                            STALINKS ID 
                                             <b>{{ info.ids }}</b>
                                         </td>
                                     </tr>
