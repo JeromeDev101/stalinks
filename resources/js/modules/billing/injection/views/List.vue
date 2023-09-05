e2a9b3c8f42c97eb212fcd1eed65c0bb1ddede20
jerome-apacaff <60372815+jerome-apacaff@users.noreply.github.com>
2023-09-05 08:43:16 +0800

    change injection id text (#1610)

--- resources/js/modules/billing/injection/views/List.vue
+++ resources/js/modules/billing/injection/views/List.vue
@@ -280,7 +280,7 @@
                                     </tr>
                                     <tr>
                                         <td colspan="2">
-                                            STALINKS ID
+                                            INJECTION ID: 
                                             <b>{{ info.ids }}</b>
                                         </td>
                                     </tr>
