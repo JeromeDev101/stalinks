a606b9a8a53a38dccb6521bdad435efb951e0e03
Yesi-MCM <yesi.marketingcrossmedia@gmail.com>
2024-03-28 07:01:48 +0800

    Update BuyController.php

--- app/Http/Controllers/BuyController.php
+++ app/Http/Controllers/BuyController.php
@@ -125,7 +125,11 @@ class BuyController extends Controller
                 $query->whereIn('user_id', $backlink_interested_user)
                     ->orderByRaw("FIELD(user_id, $orderedUserIds)");
             }])
-            ->with(['buyer_purchased.buyer.registration'])
+            ->with(['buyer_purchased.buyer.registration' => function ($query) {
+                $query->where('affiliate_id', auth()->user()->id)
+                ->where('status', 'active')
+                ->get();
+            }])
             ->leftJoin('backlinks_interesteds', function ($q) use ($user_id, $backlink_interested_user, $filter) {
                 if (isset($filter['interested_domain_name']) && !empty($filter['interested_domain_name'])) {
                     $q->on('publisher.id', '=', 'backlinks_interesteds.publisher_id')
